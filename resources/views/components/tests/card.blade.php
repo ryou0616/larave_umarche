d619b2004b1880b08a97e42253f3c82a454a2c3c
ryou0616 <matsuzaki6160@gmail.com>
2022-11-11 23:28:52 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,6 +1,8 @@
 @props(['title', 'message' => '初期値です。', 'content' => '初期値です。'])
 
-<div class="w-1/4 p-2 border-2 shadow-md">
+<div {{ $attributes->merge([
+    'class' => 'w-1/4 p-2 border-2 shadow-md'
+]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div> {{ $content }} </div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     コンポーネントテスト1
     <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル"></x-tests.card>
+    <x-tests.card title="CSSを変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
